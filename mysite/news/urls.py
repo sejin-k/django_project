7fd0140b46ea44330b12561b34c084c4d474af44
sejink <saejin7649@gmail.com>
2019-05-12 18:03:18 +0900

    Add the news application

---
+++ mysite/news/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('<int:year>/', views.year_archive),
+    path('<int:year>/<int:month>/', views.month_archive),
+    path('<int:year>/<int:month>/<int:pk>/', views.article_detail),
+]
